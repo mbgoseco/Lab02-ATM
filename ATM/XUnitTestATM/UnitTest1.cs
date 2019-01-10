84c6e542477e8daa2df12c5e9b1b4503e5e6500c
mbgoseco <mbgoseco@gmail.com>
2019-01-09 17:06:12 -0800

    updated readme, refactored tests

--- ATM/XUnitTestATM/UnitTest1.cs
+++ ATM/XUnitTestATM/UnitTest1.cs
@@ -10,34 +10,34 @@ namespace XUnitTestATM
         [Fact]
         public void AddTenToBalance()
         {
-            Assert.Equal(Convert.ToUInt32("5010"), Program.Deposit(5000, 10));
+            Assert.Equal(5010, (int)Program.Deposit(5000, 10));
         }
         [Fact]
         public void AddThousandToZeroBalance()
         {
-            Assert.Equal(Convert.ToUInt32("1000"), Program.Deposit(0, 1000));
+            Assert.Equal(1000, (int)Program.Deposit(0, 1000));
         }
         [Fact]
         public void AddZeroToZeroBalance()
         {
-            Assert.Equal(Convert.ToUInt32("0"), Program.Deposit(0, 0));
+            Assert.Equal(0, (int)Program.Deposit(0, 0));
         }
 
         // Tests for Withdraw method
         [Fact]
         public void MinusFiveFromBalance()
         {
-            Assert.Equal(Convert.ToUInt32("745"), Program.Withdraw(750, 5));
+            Assert.Equal(745, (int)Program.Withdraw(750, 5));
         }
         [Fact]
         public void MinusZeroFromBalance()
         {
-            Assert.Equal(Convert.ToUInt32("1000"), Program.Withdraw(1000, 0));
+            Assert.Equal(1000, (int)Program.Withdraw(1000, 0));
         }
         [Fact]
-        public void MinusZeroFromZero()
+        public void MinusTenFromZeroBalance()
         {
-            Assert.Equal(Convert.ToUInt32("0"), Program.Withdraw(0, 0));
+            Assert.Equal(-10, (int)Program.Withdraw(0, 10));
         }
     }
 }
